55a4004aeb50cf801bd39512b4ee73f896b8f86d
Nicolas Rime <n.rime@hotmail.com>
2024-12-06 07:02:27 +0100

    bitree representation

--- program.cpp
+++ program.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 
 void test_bitree() {
-    vector<int> v1 = {2,1,5};
+    vector<int> v1 = {2,1,5,4,9,11,34,3,4,7,19};
+    print_vector(v1);
 
     BIT b1 = construct_BIT(v1);
-    cout << b1.rsq(0,2) << endl;
-    cout << b1.rsq(0,3) << endl;
+//    cout << b1.rsq(0,2) << endl;
+
+    cout << b1.repr() << endl;
 }
 
 
 int main() {
+
 
     test_bitree();
 
--- util.h
+++ util.h
@@ -30,7 +30,6 @@ void print_vector(std::vector<T> v) {
 std::string bin_repr(int n) {
     bool is_neg(n < 0);
     if (is_neg) n = -n;
-    std::cout << n << std::endl;
     std::string repr;
     while(n > 0) {
         if (n & 1) repr = "1" + repr;
@@ -72,6 +71,31 @@ class BIT {
                 idx -= idx & (-idx);
             }
             return val;
+        }
+
+        std::string repr() const {
+            std::string repr;
+            std::string sep = " | ";
+            std::string el;
+            std::unordered_map<int,std::string> idx_list;
+            int idx(0);
+
+            for (int i(1); i < data.size(); i++) {
+                idx = i;
+                while (idx < data.size()) {
+                    idx_list[idx] += std::to_string(i-1) + ",";
+                    idx += idx & (-idx);
+                }
+            }
+
+            // idx 0 is dummy node
+            for (int i(1); i < data.size(); i++) {
+                el = std::to_string(data[i]);
+                el.insert(0, 10 - el.size(), ' ');
+                repr += bin_repr(i) + sep + el + sep + "f(" + idx_list[i];
+                repr.replace(repr.size()-1, 1, ")\n");
+            }
+            return repr;
         }
 };
 
